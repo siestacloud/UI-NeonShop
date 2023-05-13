e1f9e3f0a02d053ce65c0ed56cf0ea82c6ab46ba
Poul Sunny <siesta.cloud@gmail.com>
2023-05-13 21:04:32 +0300

    ok

--- src/components/common/banner/banner.tsx
+++ src/components/common/banner/banner.tsx
@@ -1,5 +1,5 @@
-import { Skills } from "../skills/skills";
-import ship from "./img/img.svg";
+import { About } from "../about/about";
+import { Stickers } from "../stickers/stickers";
 // Board компонент отображает отдельный board с issues
 export function Banner() {
 
@@ -7,18 +7,8 @@ export function Banner() {
     <article className="banner" >
       <div className="banner__wrapper">
 
-        <div className="banner__about">
-          <div className="banner__text">
-            <p className="banner__welcome">Welcome to site</p>
-            <h1 className="banner__headline">Hi! this is layout template</h1>
-            <p className="banner__desc">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Perferendis, obcaecati aliquid! Quasi, maxime? Vero quis obcaecati aut distinctio iure, totam fuga. Enim inventore officia perspiciatis magni quae quas qui esse voluptate nihil minus iure, harum nisi eius repellendus, corporis aliquid.</p>
-            <button className="banner__btn">Let's connect!
-              <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" width="25" height="25" fill="currentColor"><path fill-rule="evenodd" d="M1 8a7 7 0 1 0 14 0A7 7 0 0 0 1 8zm15 0A8 8 0 1 1 0 8a8 8 0 0 1 16 0zM4.5 7.5a.5.5 0 0 0 0 1h5.793l-2.147 2.146a.5.5 0 0 0 .708.708l3-3a.5.5 0 0 0 0-.708l-3-3a.5.5 0 1 0-.708.708L10.293 7.5H4.5z"></path></svg>
-            </button>
-          </div>
-          <img src={ship} className="banner__img"></img>
-        </div>
-        <Skills></Skills>
+        <About></About>
+        <Stickers></Stickers>
 
 
       </div>
---
+++ src/components/common/stickers/stickers.tsx
@@ -0,0 +1,48 @@
+import a from "./img/1.png";
+import b from "./img/2.png";
+import c from "./img/3.png";
+import d from "./img/4.png";
+import Carousel from 'react-multi-carousel';
+import 'react-multi-carousel/lib/styles.css';
+
+
+// Board компонент отображает отдельный board с issues
+export function Stickers() {
+
+  const responsive = {
+    superLargeDesktop: {
+      // the naming can be any, depends on you.
+      breakpoint: { max: 4000, min: 3000 },
+      items: 5
+    },
+    desktop: {
+      breakpoint: { max: 3000, min: 1024 },
+      items: 3
+    },
+    tablet: {
+      breakpoint: { max: 1024, min: 464 },
+      items: 2
+    },
+    mobile: {
+      breakpoint: { max: 464, min: 0 },
+      items: 1
+    }
+  };
+
+  return (
+    <article className="stickers" >
+      <h2 className="stickers__headline">Stickers</h2>
+      <p className="stickers__desc">Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur delectus odit ab facere culpa. <br /> Voluptas eos fugit vitae ut iure?</p>
+
+      <Carousel responsive={responsive} infinite={true} className="owl-carousel owl-theme stickers__gallery">
+        <img src={a} alt="ok"></img>
+        <img src={b} alt="ok"></img>
+        <img src={c} alt="ok"></img>
+        <img src={d} alt="ok"></img>
+      </Carousel>
+    </article>
+  );
+}
+
+
+
