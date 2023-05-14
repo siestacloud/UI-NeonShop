667352316bdfe2faaff7fe5690e080d3dd8def1f
Poul Sunny <siesta.cloud@gmail.com>
2023-05-14 15:37:42 +0300

    ok

--- src/components/goods/footer.tsx
+++ src/components/goods/footer.tsx
@@ -1,11 +1,19 @@
 import { useAppState } from '../../hooks/appHook';
+import { Logo } from '../common/logo/logo';
+import { Social } from '../common/social/social';
+import { Subscribe } from '../common/subscribe/subscibe';
 
 function Footer() {
 
 
   return (
     <footer className="footer">
-      <div className="footer__row">
+      <div className="footer__wrapper">
+        <Subscribe></Subscribe>
+        <div className='footer__row'>
+          <Logo></Logo>
+          <Social></Social>
+        </div>
 
       </div>
     </footer>
--- src/components/goods/main.tsx
+++ src/components/goods/main.tsx
@@ -12,7 +12,6 @@ function Main() {
       <Gallery></Gallery>
       {/* <Gallery></Gallery> */}
       <Contact></Contact>
-      <Subscribe></Subscribe>
     </main>
   );
 }
